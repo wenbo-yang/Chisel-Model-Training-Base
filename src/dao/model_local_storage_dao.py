23d32179eca1daaa94acac320c9c783e9790e337
wenbo.yang <wenbo.yang.84@gmail.com>
2024-07-29 20:23:59 -0700

    add test

--- src/dao/model_local_storage_dao.py
+++ src/dao/model_local_storage_dao.py
@@ -1,6 +1,24 @@
+from genericpath import isfile
+import json
+import os
 from dao.model_storage_dao import ModelStorageDao
 
 class ModelLocalStorageDao(ModelStorageDao):
     def __init__(self, config):
         self.__config = config
+        self.__folder_path = os.path.join(self.__config.storage_url, '/model')
+        return
+    
+    async def get_latest_model(self):
+        executions = await self.__get_executions()
+
+        return
+    
+
+    async def __get_executions(self):
+        files = [f for f in os.listdir(self.__folder_path) if isfile(os.path.join(self.__folder_path, f))]
+
+        for f in files:
+            execution = ModelTrainingExecution(json.load(f))
+
         return
---
+++ src/types/model_training_execution.py
@@ -0,0 +1,9 @@
+class ModelTrainingExecution:
+    def __init__(self, model_training_execution_json):
+        self.__model_training_execution_json = model_training_execution_json
+        return
+    
+    
+    
+
+    
